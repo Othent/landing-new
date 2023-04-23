0286d03ec1c0caf6137004371d4b08e6aa3b7e1b
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-24 00:51:55 +0100

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <meta name='description' content='Merging Web2 to Web3 user logins with a familiar and simple interface.' />
         <link rel='icon' href='/favicon.ico' />
         <meta property="og:title" content=">Othent - Merging Web2 to Web3 user logins with a familiar and simple interface."/>
-        <meta property="og:url" content="http://Othent.io/"/>
+        <meta property="og:url" content="http://Othent.io"/>
         <meta property="og:image" content="/favicon.ico"/>
         <meta property="og:image:width" content="200"/>
         <meta property="og:image:height" content="200"/>
