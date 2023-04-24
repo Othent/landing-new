8a6bed930f360679ec413d4ad16ac660d10bb0cc
Marton Lederer <marton@lederer.hu>
2023-04-24 19:43:04 +0200

    feat: analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script'
 import { ThemeProvider, DefaultTheme } from 'styled-components';
 import GlobalStyle from '../components/globalstyles';
 import Nav from '../components/Nav';
@@ -18,6 +19,19 @@ export default function App({ Component, pageProps }: AppProps) {
         <Nav />
         <Component {...pageProps} />
       </ThemeProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MH1E8F6681"
+        async
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MH1E8F6681');
+        `}
+      </Script>
     </>
   );
 }
