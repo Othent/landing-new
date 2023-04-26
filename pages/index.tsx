5d9f353150cb3db62fe76eb3a7c2f2c233e9041b
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-26 01:23:43 +0100

    fix: og title

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,11 +24,11 @@ export default function Home() {
     <SectionOne>
       <Head>
         <title>Othent - Merging Web2 to Web3 user logins with a familiar and simple interface.</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='https://Othent.io/favicon.ico' />
         <meta property="og:title" content="Othent - Merging Web2 to Web3 user logins with a familiar and simple interface."/>
         <meta property='og:description' content='Merging Web2 to Web3 user logins with a familiar and simple interface.' />
-        <meta property="og:url" content="http://Othent.io"/>
-        <meta property="og:image" content="/favicon.ico"/>
+        <meta property="og:url" content="https://Othent.io"/>
+        <meta property="og:image" content="https://Othent.io/favicon.ico"/>
         <meta property="og:image:width" content="200"/>
         <meta property="og:image:height" content="200"/>
         <meta name="keywords" content="Othent, Smart Contract Wallets, Web3, JWT Transactions, Arweave, Community Labs"></meta>
@@ -37,6 +37,8 @@ export default function Home() {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta name="theme-color" content="#2375EF"></meta>
+        <meta name="twitter:site" content="@othent_io"></meta>
+        <meta property="og:site_name" content="Othent"></meta>
       </Head>
 
       <Page>
