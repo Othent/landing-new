886a903b6c07a13e8fb505eac7d2d9d746cdf0c1
7i7o <mparij@communitylabs.com>
2024-09-04 11:50:03 +0200

    fix deps + remove unused files

--- components/SDKSection/index.tsx
+++ components/SDKSection/index.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { FeatureTextSmall, CustomHeaderText } from '../common';
-import { SpaceGrotesk700, DMSans700 } from '../../utils/fonts';
-import * as Styled from './styles';
-import Button from '../Button';
-import { Othent } from 'othent';
-import Highlight from 'react-highlight';
-import 'highlight.js/styles/tomorrow-night-bright.css';
-
-
-
+import React, { useState } from "react";
+import { FeatureTextSmall, CustomHeaderText } from "../common";
+import { SpaceGrotesk700, DMSans700 } from "../../utils/fonts";
+import * as Styled from "./styles";
+import Highlight from "react-highlight";
+import "highlight.js/styles/tomorrow-night-bright.css";
 
 const SDKSection = () => {
-
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [API_ID, setAPI_ID] = useState("");
-
   const [copiedPopup, setCopiedPopup] = useState(false);
   const handleCopyPopup = () => {
     setCopiedPopup(true);
@@ -23,19 +14,7 @@ const SDKSection = () => {
     }, 500);
   };
 
-  async function getAPIKey() {
-    try {
-      const othent = await Othent({ API_ID: 'd7a29242f7fdede654171a0d3fd25163' });
-      let { API_ID } = await othent.getAPIID();
-      setAPI_ID(API_ID);
-      setIsPopupOpen(true);
-    } catch (error) {
-      alert('Please create a Othent account (in the navbar)')
-      // if no Othent account maybe we should just create one there and then for them
-    }
-  }
-
-
+  // TODO: Replace this example with KMS usage
   const codeString = `import { Othent } from 'othent';
 
 const othent = await Othent({ API_ID });
@@ -50,68 +29,17 @@ const transaction = await othent.signTransactionArweave({
 
 const tx = await othent.sendTransactionArweave(transaction);
 
-console.log(\`Transaction ID \i\s : \${tx.transactionId}\`);`
-
-  
-
-  useEffect(() => {
-
-    const handleClickOutside = (event) => {
-      if (event.target.classList.contains('popup-background')) {
-        setIsPopupOpen(false);
-      }
-    };
-
-    document.addEventListener('click', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-
-
-
-  const [copyAPIIDClicked, setCopyAPIIDClicked] = useState(false);
-  const handleAPIIdCopy = () => {
-    setCopyAPIIDClicked(true);
-    navigator.clipboard.writeText(API_ID);
-    handleCopyPopup()
-    setTimeout(() => {
-      setCopyAPIIDClicked(false);
-    }, 100);
-  };
-
+console.log(\`Transaction ID \i\s : \${tx.transactionId}\`);`;
 
   const copyCode = () => {
     navigator.clipboard.writeText(codeString);
-    handleCopyPopup()
-  }
-
-
-  // const [callbackURL, setCallbackURL] = useState('');
-  // const [URLMessage, setURLMessage] = useState('');
-  // async function addCallbackURL() {
-  //   try {
-  //     const othent = await Othent({ API_ID: 'd7a29242f7fdede654171a0d3fd25163' });
-  //     await othent.addCallbackURL({ callbackURL: callbackURL });
-  //     setURLMessage('Success');
-  //   } catch (error) {
-  //     setURLMessage('Failed, please make sure format matches https://othent.io and try again');
-  //   }
-  // }
-  // function handleCallbackURLChange(event) {
-  //   setCallbackURL(event.target.value);
-  //   setURLMessage('');
-  // }
-
-
+    handleCopyPopup();
+  };
 
   return (
     <Styled.MainContainer>
       {copiedPopup && (
-        <Styled.copiedPopup className="popup">
-          Text copied!
-        </Styled.copiedPopup>
+        <Styled.copiedPopup className="popup">Text copied!</Styled.copiedPopup>
       )}
       <FeatureTextSmall className={DMSans700.className}>
         Bring dApp’s from 0 to 1
@@ -131,96 +59,50 @@ console.log(\`Transaction ID \i\s : \${tx.transactionId}\`);`
 
           <Styled.IconsContainer>
             <div>
-              <img src='/link-icon.svg' alt='link icon' draggable={false} />
+              <img src="/link-icon.svg" alt="link icon" draggable={false} />
               <p className={DMSans700.className}>Chains</p>
             </div>
             <div>
-              <img src='/code-icon.svg' alt='link icon' draggable={false} />
+              <img src="/code-icon.svg" alt="link icon" draggable={false} />
               <p className={DMSans700.className}>dApps</p>
             </div>
             <div>
-              <img src='/wallet-icon.svg' alt='link icon' draggable={false} />
+              <img src="/wallet-icon.svg" alt="link icon" draggable={false} />
               <p className={DMSans700.className}>Wallets</p>
-              </div>
+            </div>
           </Styled.IconsContainer>
-          {/* <Button onClick={() => getAPIKey()}>
-            Get your API ID
-          </Button> */}
+        </Styled.Onboard>
 
-
-        {isPopupOpen && (
-          <>
-            <Styled.BlurredBody className="popup-background">
-              
-              <Styled.Popup>
-
-                <Styled.PopupHeaderContainer>
-                  <Styled.PopupHeader>Your API ID</Styled.PopupHeader>
-                  <Styled.PopupCloseButton onClick={() => setIsPopupOpen(false)}>
-                    Close
-                  </Styled.PopupCloseButton>
-                </Styled.PopupHeaderContainer>
-
-                <Styled.PopupBody>
-                  
-                  <Styled.APIDetailsContainer>
-                    <Styled.ApiKeyIdLabel>API ID:</Styled.ApiKeyIdLabel>
-                    <Styled.ApiKeyIdValue>{API_ID}</Styled.ApiKeyIdValue>
-                    <Styled.APICopy
-                      src="./copy.svg"
-                      alt="Copy icon"
-                      onClick={handleAPIIdCopy}
-                      style={{ filter: copyAPIIDClicked ? "grayscale(100%) brightness(0%)" : "none" }}
-                    />
-                  </Styled.APIDetailsContainer>
-
-                  {/* <Styled.AddURLContainer>
-                    <Styled.AddURL
-                      placeholder="Add callback URLs, eg https://othent.io"
-                      value={callbackURL}
-                      onChange={handleCallbackURLChange}
-                    />
-                    <Styled.AddURLButton onClick={addCallbackURL}>Add callback URL</Styled.AddURLButton>
-                    {URLMessage && (
-                      <Styled.URLMessage style={{ color: URLMessage === 'Success' ? 'green' : 'red' }}>
-                        {URLMessage}
-                      </Styled.URLMessage>
-                    )}
-                  </Styled.AddURLContainer> */}
-
-                </Styled.PopupBody>
-              </Styled.Popup>
-            </Styled.BlurredBody> 
-            
-          </>
-        )}
-
-
-      </Styled.Onboard>
-
-      <div>
-        <div className='code-block-header'>
-          <div className='header-buttons-container'>
-            <div style={{ backgroundColor: 'rgb(255, 94, 87)' }} className="header-buttons"></div>
-            <div style={{ backgroundColor: 'rgb(255, 187, 46)' }} className="header-buttons"></div>
-            <div style={{ backgroundColor: 'rgb(56, 193, 73)' }} className="header-buttons"></div>
+        <div>
+          <div className="code-block-header">
+            <div className="header-buttons-container">
+              <div
+                style={{ backgroundColor: "rgb(255, 94, 87)" }}
+                className="header-buttons"
+              ></div>
+              <div
+                style={{ backgroundColor: "rgb(255, 187, 46)" }}
+                className="header-buttons"
+              ></div>
+              <div
+                style={{ backgroundColor: "rgb(56, 193, 73)" }}
+                className="header-buttons"
+              ></div>
+            </div>
+            <button className="copy-code" onClick={copyCode}>
+              <img
+                src="./copy-black.svg"
+                alt="Copy icon"
+                className="copy-code-img"
+              />
+              <span>Copy</span>
+            </button>
           </div>
-          <button className='copy-code' onClick={copyCode}>
-            <img src="./copy-black.svg" alt="Copy icon" className='copy-code-img' />
-            <span>Copy</span>
-          </button>
+          <Highlight className="code-block">{codeString}</Highlight>
         </div>
-        <Highlight className='code-block'>
-          {codeString}
-        </Highlight>
-      </div>
-
-      
-
-  </Styled.OnboardContainer>
-</Styled.MainContainer>
-
-);
+      </Styled.OnboardContainer>
+    </Styled.MainContainer>
+  );
 };
 
 export default SDKSection;
--- components/WeaveTransfer/index.tsx
+++ components/WeaveTransfer/index.tsx
@@ -1,36 +1,49 @@
-import { useState } from 'react';
-import { FeatureTextSmall } from '../common';
+import { ChangeEvent, DragEventHandler, useMemo, useState } from "react";
+import { FeatureTextSmall } from "../common";
 
-import { DMSans700, SpaceGrotesk700, DMSans500 } from '../../utils/fonts';
-import * as Styled from './styles';
-import Button from '../Button';
-import { Othent } from 'othent';
+import { DMSans700, SpaceGrotesk700, DMSans500 } from "../../utils/fonts";
+import * as Styled from "./styles";
+import Button from "../Button";
+// import { Othent } from 'othent';
+import { Othent, AppInfo } from "@othent/kms";
+import Arweave from "arweave";
+
+const appInfo: AppInfo = {
+  name: "WeaveTransfer",
+  version: "1.0.0",
+  env: "production",
+};
 
 const WeaveTransfer = () => {
-  const [menuActive, setMenuActive] = useState('upload');
+  const [menuActive, setMenuActive] = useState("upload");
   const [downloadDemo, setDownloadDemo] = useState(false);
   const [loading, setLoading] = useState(false);
   const [requestStatus, setRequestStatus] = useState("");
-  const [file, setFile] = useState(null);
-  const [fileName, setFileName] = useState("");
+  const [file, setFile] = useState<null | File>(null);
+  const arweave = useMemo(() => {
+    return Arweave.init({
+      host: "ar-io.net",
+      port: 443,
+      protocol: "https",
+    });
+  }, []);
 
   function handleFileUpload(event) {
     const file = event.target.files[0];
     setFile(file);
-    setFileName(file.name);
   }
 
   const [txnInputValue, setTxnInputValue] = useState("");
-  const [downloadTransactionId, setDownloadTransactionId] = useState('');
+  const [downloadTransactionId, setDownloadTransactionId] = useState("");
 
   function downloadWTLink() {
     if (!downloadTransactionId) {
       alert("Please enter a transaction id");
     }
-    window.open('https://arweave.net/' + downloadTransactionId)
+    window.open("https://arweave.net/" + downloadTransactionId);
   }
 
-  const [sendToEmail, setSendToEmail] = useState("")
+  const [sendToEmail, setSendToEmail] = useState("");
   const [userEmail, setUserEmail] = useState(null);
 
   function isValidEmail(email) {
@@ -53,67 +66,97 @@ const WeaveTransfer = () => {
     }
 
     if (!isValidEmail(sendToEmail)) {
-      alert('Please enter a valid recipient email address');
+      alert("Please enter a valid recipient email address");
       return;
     }
 
-    setLoading(true)
+    setLoading(true);
 
-    const othentInstance = await Othent({ API_ID: 'd7a29242f7fdede654171a0d3fd25163' });
+    const othent = new Othent({ appInfo });
 
-    let user_details
+    await othent.requireAuth();
 
-    if (JSON.parse(localStorage.getItem('othentUserDetails'))) {
-      user_details = await othentInstance.userDetails()
-    } else {
-      // @ts-ignore
-      user_details = await othentInstance.logIn()
-      localStorage.setItem('othentUserDetails', JSON.stringify(user_details));
+    const tx = await arweave.createTransaction({
+      data: await file.arrayBuffer(),
+    });
+    tx.addTag("Content-Type", file.type);
+
+    const signedTx = await othent.sign(tx);
+
+    // console.log(signedTx);
+
+    console.log(
+      `The signature from 'othent.sign(tx)' is ${
+        (await arweave.transactions.verify(signedTx))
+          ? "valid ✅"
+          : "invalid ❌"
+      }`
+    );
+
+    const postedTx = await arweave.transactions.post(signedTx);
+
+    if (postedTx.status > 200) {
+      console.error(
+        `Transaction could not be posted. Response: ${postedTx.status} - ${postedTx.statusText}`
+      );
+      setRequestStatus("failed");
+      setLoading(false);
+      return;
     }
 
-    if (user_details.message === 'new user created') {
-      alert('New Othent account created! Please re send the file')
-      setLoading(false)
-    } else {
+    // TEST: Set the signature in the original tx
+    // tx.setSignature({
+    //   id: signedTx.id,
+    //   owner: signedTx.owner,
+    //   reward: signedTx.reward,
+    //   tags: signedTx.tags,
+    //   signature: signedTx.signature,
+    // });
+    // console.log(
+    //   `The signature from 'othent.sign(tx)' is ${
+    //     (await arweave.transactions.verify(tx)) ? "valid ✅" : "invalid ❌"
+    //   }`
+    // );
+    // const postedTxB = await arweave.transactions.post(tx);
+    // if (postedTxB.status > 200) {
+    //   console.error(
+    //     `Transaction could not be posted. Response: ${postedTxB.status} - ${postedTxB.statusText}`
+    //   );
+    //   setRequestStatus("failed");
+    //   setLoading(false);
+    //   return;
+    // }
 
-      const signedArweaveTransaction = await othentInstance.signTransactionArweave({
-        othentFunction: 'uploadData',
-        data: file,
-        tags: [ {name: 'Content-Type', value: file.type} ]
-      });
+    const user_details = await othent.getUserDetails();
 
-      const transaction = await othentInstance.sendTransactionArweave(signedArweaveTransaction);
+    const formData = new FormData();
+    formData.append("transaction_id", signedTx.id);
+    formData.append("sendToEmail", sendToEmail);
+    formData.append("sendFromEmail", user_details.email);
 
-      const formData = new FormData();
-      formData.append("transaction_id", transaction.transactionId);
-      formData.append("sendToEmail", sendToEmail);
-      formData.append("sendFromEmail", user_details.email);
-
-      fetch('https://server.othent.io/weavetransfer', {
-        method: 'POST',
-        body: formData,
+    fetch("https://server.othent.io/weavetransfer", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setSendToEmail("");
+        setFile(null);
+        if (data.success === true) {
+          setRequestStatus("success");
+          setTransactionId(signedTx.id);
+          setUserEmail(user_details.email);
+          setWalletAddress(user_details.walletAddress);
+        } else {
+          setRequestStatus("failed");
+        }
       })
-        .then((response) => response.json())
-        .then((data) => {
-          setLoading(false);
-          setFileName("");
-          setSendToEmail("")
-          setFile(null);
-          if (data.success === true) {
-            setRequestStatus('success');
-            setTransactionId(transaction.transactionId);
-            setUserEmail(user_details.email)
-            setWalletAddress(user_details.contract_id)
-          } else {
-            setRequestStatus('failed');
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          setRequestStatus('failed');
-          setLoading(false);
-        });
-    }
+      .catch((error) => {
+        console.error(error);
+        setRequestStatus("failed");
+        setLoading(false);
+      });
   }
 
   return (
@@ -125,10 +168,14 @@ const WeaveTransfer = () => {
         Experience it yourself
       </Styled.HeaderText>
 
-      <Styled.Container >
+      <Styled.Container>
         <Styled.WTContainer>
-          <div className='weave-transfer'>
-            <img src='/wt-logo.svg' alt='weave transfer logo' draggable={false} />
+          <div className="weave-transfer">
+            <img
+              src="/wt-logo.svg"
+              alt="weave transfer logo"
+              draggable={false}
+            />
             <p className={SpaceGrotesk700.className}>Weave Transfer</p>
           </div>
 
@@ -137,137 +184,156 @@ const WeaveTransfer = () => {
           </p>
 
           <Styled.UploadMenu active={menuActive}>
-            <div className='menu-items'>
+            <div className="menu-items">
               <p
                 className={`${DMSans700.className} upload-text`}
                 onClick={() => {
-                  setTxnInputValue('');
-                  setMenuActive('upload');
+                  setTxnInputValue("");
+                  setMenuActive("upload");
                 }}
               >
                 Upload
               </p>
               <p
                 className={`${DMSans700.className} download-text`}
-                onClick={() => setMenuActive('download')}
+                onClick={() => setMenuActive("download")}
               >
                 Download
               </p>
             </div>
-            <div className='progress'>
-              <div className='indicator' />
+            <div className="progress">
+              <div className="indicator" />
             </div>
           </Styled.UploadMenu>
 
-          {menuActive === 'upload' ? (
+          {menuActive === "upload" ? (
             <>
-            {!(transaction_id && walletAddress) && (
-              <>
-                <label
-                  onDragOver={handleFileUpload}
-                  onDrop={handleFileUpload}
-                  className={`${DMSans700.className} file-upload`}
-                  htmlFor="file-input">
+              {!(transaction_id && walletAddress) && (
+                <>
+                  <label
+                    onDragOver={handleFileUpload}
+                    onDrop={handleFileUpload}
+                    className={`${DMSans700.className} file-upload`}
+                    htmlFor="file-input"
+                  >
+                    <span
+                      className="upload-icon"
+                      role="img"
+                      aria-label="upload icon"
+                    >
+                      {file?.name ? "✅" : "📁"}
+                    </span>
+                    <span className="upload-text">
+                      {file?.name ? file.name : "Choose a file or drag it here"}
+                    </span>
+                  </label>
+                  <input
+                    id="file-input"
+                    type="file"
+                    onChange={handleFileUpload}
+                  />
 
-                  <span className="upload-icon" role="img" aria-label="upload icon">
-                    {fileName ? "✅" : "📁"}
-                  </span>
-                  <span className="upload-text">
-                    {fileName ? fileName : "Choose a file or drag it here"}
-                  </span>
+                  <input
+                    type="text"
+                    placeholder="Recipient email"
+                    className={`${DMSans500.className} upload-text`}
+                    value={sendToEmail}
+                    onChange={(event) => setSendToEmail(event.target.value)}
+                  />
+                </>
+              )}
 
-                </label>
-                <input id="file-input" type="file" onChange={handleFileUpload} />
+              {transaction_id && walletAddress && (
+                <>
+                  <p className="upload-profile">
+                    <b>Successfully sent with, </b>
+                    <span className="sent-with-email">{userEmail}</span>
+                  </p>
+                  <p className="id-wallet-upload">
+                    <b>Transaction ID: </b>
+                    <span>
+                      <a
+                        className="txn-id-a"
+                        href={"https://arweave.net/" + transaction_id}
+                        target="_blank"
+                      >
+                        {transaction_id}
+                      </a>
+                    </span>
+                  </p>
 
-                <input
-                  type='text'
-                  placeholder='Recipient email'
-                  className={`${DMSans500.className} upload-text`}
-                  value={sendToEmail}
-                  onChange={(event) => setSendToEmail(event.target.value)}
-                />
-              </>
-            )}
+                  <p className="id-wallet-upload">
+                    <b>Your Wallet Address: </b>
+                    <a
+                      className="txn-id-a"
+                      href={
+                        "https://sonar.warp.cc/#/app/contract/" + walletAddress
+                      }
+                      target="_blank"
+                    >
+                      {walletAddress}
+                    </a>
+                  </p>
 
-            {transaction_id && walletAddress && (
-              <>
-                <p className='upload-profile'>
-                  <b>Successfully sent with, </b>
-                  <span className='sent-with-email'>{userEmail}</span>
-                </p>
-                <p className='id-wallet-upload'>
-                  <b>Transaction ID: </b>
-                  <span>
-                    <a className='txn-id-a' href={'https://arweave.net/' + transaction_id} target="_blank">{transaction_id}</a>
-                  </span>
-                </p>
+                  <Button fullWidth onClick={() => window.location.reload()}>
+                    <img
+                      src="/refresh.svg"
+                      alt="Refresh icon"
+                      draggable={false}
+                    />
+                    Send another file
+                  </Button>
+                </>
+              )}
 
-                <p className='id-wallet-upload'>
-                  <b>Your Wallet Address: </b>
-                  <a className='txn-id-a' href={'https://sonar.warp.cc/#/app/contract/' + walletAddress} target="_blank">{walletAddress}</a>
-                </p>
-
-                <Button fullWidth onClick={() => window.location.reload()}>
-                  <img src="/refresh.svg" alt="Refresh icon" draggable={false} />
-                  Send another file
+              {!(transaction_id && walletAddress) && (
+                <Button fullWidth onClick={uploadFileButton}>
+                  {loading ? (
+                    <>
+                      <img
+                        src="/spinner.gif"
+                        alt="Spinner icon"
+                        draggable={false}
+                      />
+                      <span>Loading...</span>
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        src="/wt-google.svg"
+                        alt="Google icon"
+                        draggable={false}
+                      />
+                      Send on chain with Google
+                    </>
+                  )}
                 </Button>
-              </>
-
-            )}
-
-            {!(transaction_id && walletAddress) && (
-              <Button fullWidth onClick={uploadFileButton}>
-                {loading ? (
-                  <>
-                    <img src="/spinner.gif" alt="Spinner icon" draggable={false} />
-                    <span>Loading...</span>
-                  </>
-                ) : (
-                  <>
-                    <img src="/wt-google.svg" alt="Google icon" draggable={false} />
-                    Send on chain with Google
-                  </>
-                )}
-              </Button>
-            )}
-          </>
-
-
-
-
-
-
-
-
-
-
-
+              )}
+            </>
           ) : (
             <>
-              <p className='txn-id'>Transaction ID: </p>
+              <p className="txn-id">Transaction ID: </p>
               {downloadDemo ? (
-                <img src='/downloading.svg' alt='' draggable={false} />
+                <img src="/downloading.svg" alt="" draggable={false} />
               ) : (
                 <input
-                  type='text'
-                  className='txn-input'
-                  placeholder='Enter your Transaction ID here...'
+                  type="text"
+                  className="txn-input"
+                  placeholder="Enter your Transaction ID here..."
                   value={downloadTransactionId}
-                  onChange={(event) => setDownloadTransactionId(event.target.value)}
+                  onChange={(event) =>
+                    setDownloadTransactionId(event.target.value)
+                  }
                 />
               )}
 
               <Button onClick={downloadWTLink} fullWidth>
-                <img src='/download.svg' alt='upload icon' draggable={false} />
+                <img src="/download.svg" alt="upload icon" draggable={false} />
                 Download
               </Button>
             </>
           )}
         </Styled.WTContainer>
-
-
-
-
       </Styled.Container>
     </Styled.MainWrapper>
   );
